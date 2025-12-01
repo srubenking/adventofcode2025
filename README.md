83c6c6e0cd77aa73dfafcee92b38fcd469d5f31b
srubenking <sophie.rubenking@gmail.com>
2025-12-02 03:07:28 +1100

    update readme with first attempt info, note bug in first attempt

--- README.md
+++ README.md
@@ -3,10 +3,14 @@
 <details>
   <summary>Day 1 (spoilers!)</summary>
   
+  ## Part 1
   First attempt was a failure. Found that I wasn't accounting for rotations that brought the number lower than -100.
 
   Fixed this but still wrong.
 
   Wasn't accounting for numbers higher than 99! My fault for trying to do this at almost 3 in the morning.
+
+  ## Part 2
+  Again, first attempt failed, must be not accounting for something.
   
 </details>
--- day1/main.py
+++ day1/main.py
@@ -14,28 +14,59 @@ def main():
     dial = 50
     zeroes = 0
 
-    for rotation in instructions:
-        direction = rotation[0]
-        clicks = int(rotation[1:])
-        
-        if direction == "L":
-            # left
-            dial -= clicks
-        else:
-            # right
-            dial += clicks
+    print("Enter the password:")
+    password = input()
 
-        # account for negative numbers in general, and going < -100 with while loop
-        while dial < 0:
-            dial += 100
+    if password == "0x434C49434B":
+        for rotation in instructions:
+            direction = rotation[0]
+            clicks = int(rotation[1:])
+            
+            if direction == "L":
+                # left
+                dial -= clicks
+            else:
+                # right
+                dial += clicks
 
-        # account for number > 99 in general, and going > 199 with while loop
-        while dial > 99:
-            dial -= 100
+            # account for negative numbers in general, and going < -100 with while loop - also add each time passing 0
+            while dial < 0:
+                dial += 100
+                zeroes += 1
 
-        if dial == 0:
-            zeroes += 1
+            # account for number > 99 in general, and going > 199 with while loop - also add each time passing 0
+            while dial > 99:
+                dial -= 100
+                zeroes += 1
 
-    print(zeroes)
+            # !!! currently adding to zeroes twice if the dial lands on 100 or -99
+            if dial == 0:
+                zeroes += 1
+
+        print(f"Right password, {zeroes} zeroes")
+    else:
+        for rotation in instructions:
+            direction = rotation[0]
+            clicks = int(rotation[1:])
+            
+            if direction == "L":
+                # left
+                dial -= clicks
+            else:
+                # right
+                dial += clicks
+
+            # account for negative numbers in general, and going < -100 with while loop
+            while dial < 0:
+                dial += 100
+
+            # account for number > 99 in general, and going > 199 with while loop
+            while dial > 99:
+                dial -= 100
+
+            if dial == 0:
+                zeroes += 1
+
+        print(f"Wrong password, {zeroes} zeroes")
 
 main()
