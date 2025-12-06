7fb79bd8a338f4f12bc276e02e6b115b25f1ff6d
srubenking <sophie.rubenking@gmail.com>
2025-12-07 03:36:05 +1100

    had the correct thought before but was off by 1 (or more)

--- day3/main.py
+++ day3/main.py
@@ -31,14 +31,9 @@ def main():
     total_joltage = 0
 
     for bank in banks:
-        print(bank)
-        twelve = remove_smallest(bank, 12)
-        print(twelve)
-        input()
+        twelve = get_batteries(bank, 12)
         total_joltage += get_joltage(twelve)
 
-    # too low apparently
-    
     print(total_joltage)
 
 def get_joltage(bank):
@@ -78,6 +73,87 @@ def remove_smallest(bank, total_batteries):
                     removed += 1
     
     return new_bank
+
+def remove_smallest_debug(bank, total_batteries):
+    freq_ind = 0
+    check = freq_analysis(bank)
+    new_bank = bank
+    to_remove = len(bank) - total_batteries
+    removed = 0
+
+    print(f"to remove {to_remove} batteries")
+
+    for key, value in check.items():
+        print(f"{value} {key}s found in bank")
+        input()
+        if removed < to_remove:
+            if value == 0:
+                print("checking next number")
+                input()
+                continue
+            for batt in bank:
+                if batt == key and removed < to_remove:
+                    new_bank = new_bank.replace(key, "", 1)
+                    removed += 1
+                    print(f"{batt} found, {removed} total removed")
+                    input()
+    
+    return new_bank
+
+def get_batteries(bank, total_batteries):
+    batteries = []
+    to_remove = len(bank) - total_batteries
+
+    start = 0
+    end = to_remove + 1
+
+    # actually we just want to iterate over the whole bank checking len(bank) - 12 numbers at a time and incrementing the start by 1?
+    while len(batteries) < total_batteries:
+        #print(f"there are {to_remove} batteries to remove - checking from index {start}-{end}: {bank[start:end]}")
+        #input()
+        battery, battery_position = find_largest_p2(bank, start, end)
+        #print(f"found largest battery: battery at index {battery_position} with {battery} joltage")
+        batteries.append(battery)
+        #print(f"{"".join(batteries)}")
+        #input()
+        if battery_position != 0:
+            to_remove -= battery_position - start
+
+        start = battery_position + 1
+        end = start + to_remove + 1
+        # need to keep checking pairs until the end in case the last one is the largest
+    #print(f"final twelve found: {"".join(batteries)}")
+    """
+    while removed < to_remove:
+        print(f"there are {to_remove - removed} batteries to remove - checking from index {start}-{end}")
+        battery, battery_position = find_largest_p2(bank, start, end)
+        print(f"found largest battery: battery at index {battery_position} with {battery} joltage")
+        batteries.append(battery)
+
+        start = battery_position + 1
+        removed += len(bank[:start]) -1
+        print(f"{removed} batteries removed so far")
+
+        if len(batteries) == 12:
+            break
+
+        start = battery_position + 1
+        end = start + to_remove - removed
+        # need to keep checking pairs until the end in case the last one is the largest
+    """
+
+    return "".join(batteries)
+
+def find_largest_p2(bank, start, end):
+    largest = 0
+    largest_index = int()
+
+    for i in range(start, end):
+        if int(bank[i]) > largest:
+            largest = int(bank[i])
+            largest_index = i
+    
+    return str(largest), largest_index
 
 def read_file(file):
     f = open(file)
